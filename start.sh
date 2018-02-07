7ada2bd327b8390a2cb68d52a8348a8f382d0659
root <root@dbnode3.localdomain>
2018-02-07 10:43:01 +0800

    minor conf change from production

--- start.sh
+++ start.sh
@@ -2,10 +2,13 @@
 
 . set-env.sh
 
+# file descritor: for sockets
 ulimit -n 8192
+# defaults is 7200 which is too long
+echo 1800 > /proc/sys/net/ipv4/tcp_keepalive_time
 
-#nohup env LD_PRELOAD=libterark-zip-rocksdb-r.so:librocksdb.so \
-nohup env LD_PRELOAD=libterark-zip-rocksdb-r.so::librocksdb.so \
+
+nohup env LD_PRELOAD=libterark-zip-rocksdb-r.so:librocksdb.so \
     TerarkUseDivSufSort=1 \
     TerarkZipTable_write_buffer_size=2G \
     TerarkZipTable_indexNestLevel=4 \
@@ -22,4 +25,6 @@ nohup env LD_PRELOAD=libterark-zip-rocksdb-r.so::librocksdb.so \
     TerarkZipTable_extendedConfigFile=/root/.terark_license \
     ./ssdb-server ./chess.conf &
 
-#    TerarkZipTable_extendedConfigFile=/root/.terark_license \
+#TerarkZipTable_level0_file_num_compaction_trigger=20 \
+#HEAPCHECK=normal \
+
